c7c91114e933144683e551441fec3da845f10fe4
Karishma019 <karishmaacharya63@gmail.com>
2024-05-11 17:29:43 +0530

    added origin

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ const app = express();
 // MiddleWares
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sharezone-api-mern.vercel.app/",
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGODB_URI)
